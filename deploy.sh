b9aed2447dfb8dd2066af1611dfd23272b1500ef
NurehaFox <conorsheridan@live.ie>
2023-03-15 21:29:46 +0000

    docker changes

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ then
   docker rm node_app
 fi
 
-docker create --name node_app $IMAGE_NAME -p 8443:8443 
+docker create -p 8443:8443 --name node_app $IMAGE_NAME
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 docker cp ./privatekey.pem node_app:/privatekey.pem
