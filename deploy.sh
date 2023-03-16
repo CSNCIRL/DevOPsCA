12752c1b986a0d7eac54d9728da4089cacb20df8
NurehaFox <conorsheridan@live.ie>
2023-03-16 17:10:59 +0000

    Presentation Changes

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash 
-CURRENT_INSTANCE=$(docker ps -a -q --filter ancestor="$IMAGE_NAME:latest" --format="{{.ID}}")
+
+CURRENT_INSTANCE=$(docker ps -a -q --filter ancestor="$IMAGE_NAME:$IMAGE_TAG" --format="{{.ID}}")
 if [ "$CURRENT_INSTANCE" ]
 then
   docker rm $(docker stop $CURRENT_INSTANCE)
 fi
 
-docker pull $IMAGE_NAME:latest
+docker pull $IMAGE_NAME:$IMAGE_TAG
 
-CONTAINER_EXISTS=$(docker ps -a | grep node_app)
+CONTAINER_EXISTS=$(docker ps -a | grep $CONTAINER_NAME)
 if [ "$CONTAINER_EXISTS" ] 
 then
-  docker rm node_app
+  docker rm $CONTAINER_NAME
 fi
 
-docker create -p 8443:8443 --name node_app $IMAGE_NAME:latest
+docker create -p $PUBLIC_PORT:$INTERNAL_PORT --name $CONTAINER_NAME $IMAGE_NAME:$IMAGE_TAG
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
-docker cp ./privatekey.pem node_app:/privatekey.pem
-docker cp ./server.crt node_app:/server.crt
-docker start node_app
+docker cp ./privatekey.pem $CONTAINER_NAME:/privatekey.pem
+docker cp ./server.crt $CONTAINER_NAME:/server.crt
+docker start $CONTAINER_NAME
